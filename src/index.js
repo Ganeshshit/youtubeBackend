88647c8c7eaaeaa4bc721e17173cfb95312e8eb0
Coderganesh <gsb021@nist.edu>
2024-08-08 00:12:58 +0530

    change root directory

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 
-// import dotenv from 'dotenv'
-// import connectDB from "./db/index.js";
+import dotenv from 'dotenv'
+import connectDB from "./db/index.js";
 
-// dotenv.config({path:'./env'})
+dotenv.config({path:'./env'})
 
-// console.log("helo world")
-// connectDB()
+console.log("helo world")
+connectDB(process.env.MONGO_URI || 'mongodb+srv://gsb021:Ganesh@cluster0.77m69.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
